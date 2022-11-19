9f791bccd3ce46670e1fbe9fc7efb36ef8f869c0
saranga24 <sarangag@emojot.com>
2022-11-19 20:32:41 +0530

    saranga - save  distance between cities

--- algomrex-API-backend/src/main/java/com/pdsacw/algomrexapibackend/Service/ShortestPathService.java
+++ algomrex-API-backend/src/main/java/com/pdsacw/algomrexapibackend/Service/ShortestPathService.java
@@ -1,12 +1,13 @@
 package com.pdsacw.algomrexapibackend.Service;
 
 import com.pdsacw.algomrexapibackend.Dto.CommonUserAnswer;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 
 import java.io.UnsupportedEncodingException;
 
 public interface ShortestPathService {
 
-    ResponseEntity<Object> checkUserAnswer(CommonUserAnswer commonUserAnswer) throws UnsupportedEncodingException;
+    ResponseEntity<Object> checkUserAnswer(CommonUserAnswer commonUserAnswer, HttpHeaders headers) throws UnsupportedEncodingException;
     ResponseEntity<Object> getTableData() throws UnsupportedEncodingException;
 }
